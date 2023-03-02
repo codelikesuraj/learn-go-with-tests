b11dccfdf902b185d7b416066ca2d2696d75272e
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2023-03-02 19:11:00 +0100

    added 2_integers

---
+++ 2_integers/adder_test.go
@@ -0,0 +1,19 @@
+package integers
+
+import "fmt"
+import "testing"
+
+func TestAdder(t *testing.T) {
+	got := Add(2, 4)
+	expected := 6
+
+	if got != expected {
+		t.Errorf("expected %d but got %d ", expected, got)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
