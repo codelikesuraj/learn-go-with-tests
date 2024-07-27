d98c303d7bf31a0839693465f1dcd370b9703da2
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-07-27 10:01:29 +0100

    update: concurrency

---
+++ concurrency/website_checker.go
@@ -0,0 +1,25 @@
+package concurrency
+
+type WebsiteChecker func(string) bool
+type Result struct {
+	string
+	bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan Result)
+
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- Result{u, wc(u)}
+		}(url)
+	}
+
+	for range len(urls) {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
+	return results
+}
