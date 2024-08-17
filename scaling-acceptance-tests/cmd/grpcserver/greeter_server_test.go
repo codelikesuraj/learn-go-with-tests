5a0efbd491a2d43ce6cf0b27d26cb47bc6715105
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-08-17 13:13:22 +0100

    update: sacling acceptance tests 5

--- scaling-acceptance-tests/cmd/grpcserver/greeter_server_test.go
+++ scaling-acceptance-tests/cmd/grpcserver/greeter_server_test.go
@@ -1,20 +1,17 @@
 package main_test
 
 import (
-	"net/http"
 	"testing"
-	"time"
 
 	"github.com/codelikesuraj/learn-go-with-tests/scaling-acceptance-tests/adapters"
-	"github.com/codelikesuraj/learn-go-with-tests/scaling-acceptance-tests/adapters/httpserver"
+	"github.com/codelikesuraj/learn-go-with-tests/scaling-acceptance-tests/adapters/grpcserver"
 	"github.com/codelikesuraj/learn-go-with-tests/scaling-acceptance-tests/specifications"
 )
 
 func TestGreeterServer(t *testing.T) {
-	baseURL := adapters.StartDockerServer(t, "8080", "./cmd/httpserver/Dockerfile")
-	driver := httpserver.Driver{
-		BaseURL: baseURL,
-		Client:  &http.Client{Timeout: 1 * time.Second},
+	baseURL := adapters.StartDockerServer(t, "8080", "./cmd/grpcserver/Dockerfile")
+	driver := grpcserver.Driver{
+		Addr: baseURL,
 	}
 
 	specifications.GreetSpecification(t, driver)
